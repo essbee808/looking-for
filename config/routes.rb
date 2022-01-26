ef1d24fca56f73bf1d20605c7cb14963ebba7ed0
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-26 00:27:24 -0800

    Display user's name in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'application#welcome'
 
-  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: "omniauth_callbacks"}
+  devise_for :users, controllers: {
+    registrations: 'users/registrations', 
+    sessions: 'users/sessions',
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    root to: 'devise/sessions#home'
   end
 
   get '/auth/:provider/callback', to: 'omniauth_callbacks#google_oauth2'
