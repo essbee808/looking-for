25bd87f0523a45bf0052e7b3af281e6b6a5d680d
Sheena Bonilla <sheena.myb@gmail.com>
2022-01-27 18:59:19 -0800

    Modify associations in program model

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,7 +1,8 @@
 class Program < ApplicationRecord
-    belongs_to :user
-    belongs_to :organization
+    belongs_to :creator, class_name: "User"
     belongs_to :category
+    has_many :user_programs
+    has_many :users, through: :user_programs
 
     validates :name, :description, :website, presence: true
     validates :name, uniqueness: true
